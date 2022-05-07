48987031b8c88b0c8ea41c2f74c5beef18c517db
ADoyle <adoyle.h@gmail.com>
2022-05-08 03:00:40 +0800

    update

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -7,6 +7,14 @@ local M = {
 function M.config()
 	vim.notify = require('notify')
 	vim.api.nvim_set_keymap('n', '<space>n', ':Telescope notify<CR>', {})
+
+	vim.api.nvim_create_user_command('Notify', function(opts)
+		local args = opts.fargs
+		vim.notify(args[1], args[2] or 'info')
+	end, {
+		nargs = '+',
+		desc = "Send message to notification window. Usage: :Notify <message> [info|warn|error]",
+	})
 end
 
 return M
