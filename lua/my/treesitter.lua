4fc23a241fba4d7d6aac13390c31a27bef626691
ADoyle <adoyle.h@gmail.com>
2024-03-28 17:53:16 +0800

    fix: turn off treesitter rst

--- lua/my/treesitter.lua
+++ lua/my/treesitter.lua
@@ -10,7 +10,7 @@ return {
 		'godot_resource', 'gomod', 'gowork', 'graphql', 'glimmer', 'haskell', 'hcl', 'vimdoc', 'hjson', 'hlsl',
 		'html', 'http', 'java', 'javascript', 'jsdoc', 'json', 'json5', 'jsonnet', 'julia', 'kotlin',
 		'latex', 'llvm', 'lua', 'make', 'markdown', 'markdown_inline', 'perl', 'pioasm', 'proto',
-		'python', 'racket', 'regex', 'rst', 'rust', 'scheme', 'scss', 'solidity', 'sparql', 'sql',
+		'python', 'racket', 'regex', 'rust', 'scheme', 'scss', 'solidity', 'sparql', 'sql',
 		'svelte', 'swift', 'teal', 'toml', 'tsx', 'twig', 'typescript', 'verilog', 'vim', 'vue',
 		'wgsl', 'yaml', 'zig',
 		-- LuaFormatter on
@@ -22,7 +22,7 @@ return {
 		'foam', 'fortran', 'fusion', 'gdscript', 'gleam', 'hack', 'heex', 'hocon',
 		'lalrpop', 'ledger', 'm68k', 'menhir', 'meson', 'nickel', 'ninja', 'nix', 'ocaml',
 		'ocaml_interface', 'ocamllex', 'pascal', 'php', 'phpdoc', 'prisma', 'pug', 'ql', 'qmljs',
-		'r', 'rasi', 'rego', 'rnoweb', 'ruby', 'slint', 'supercollider', 'surface', 'sxhkdrc',
+		'r', 'rasi', 'rego', 'rst', 'rnoweb', 'ruby', 'slint', 'supercollider', 'surface', 'sxhkdrc',
 		'tiger', 'tlaplus', 'todotxt', 'turtle', 'v', 'vala', 'yang',
 		-- LuaFormatter on
 	},
