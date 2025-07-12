857e3f776c55c961ae7ff7ca8a28df1f4ec74f2e
ADoyle <adoyle.h@gmail.com>
2025-07-12 22:05:59 +0800

    update

--- lua/my/ai.lua
+++ lua/my/ai.lua
@@ -1,33 +1,33 @@
 local M = {}
 
 M.plugins = {
-	deps = {
-		{ 'yetone/avante.nvim', build = 'with-proxy build', disable = true },
-	},
+	-- deps = {
+	-- 	{ 'yetone/avante.nvim', build = 'with-proxy build', disable = true },
+	-- },
 }
 
 M.config = {
-	avante = {
-		provider = 'openai',
-		providers = {
-			openai = {
-				endpoint = 'https://api.vveai.com/v1',
-				model = 'gpt-4o-mini', -- your desired model (or use gpt-4o, etc.)
-				extra_request_body = {
-					timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-					temperature = 0,
-					max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-					--reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
-				},
-			},
-		},
+	-- avante = {
+	-- 	provider = 'openai',
+	-- 	providers = {
+	-- 		openai = {
+	-- 			endpoint = 'https://api.vveai.com/v1',
+	-- 			model = 'gpt-4o-mini', -- your desired model (or use gpt-4o, etc.)
+	-- 			extra_request_body = {
+	-- 				timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+	-- 				temperature = 0,
+	-- 				max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+	-- 				--reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+	-- 			},
+	-- 		},
+	-- 	},
 
-		-- https://github.com/yetone/avante.nvim#web-search-engines
-		web_search_engine = {
-			provider = 'brave', -- tavily, serpapi, searchapi, google, kagi, brave, or searxng
-			proxy = nil, -- proxy support, e.g., http://127.0.0.1:7890
-		},
-	},
+	-- 	-- https://github.com/yetone/avante.nvim#web-search-engines
+	-- 	web_search_engine = {
+	-- 		provider = 'brave', -- tavily, serpapi, searchapi, google, kagi, brave, or searxng
+	-- 		proxy = nil, -- proxy support, e.g., http://127.0.0.1:7890
+	-- 	},
+	-- },
 
 	codecompanion = {
 		strategies = {
@@ -42,6 +42,18 @@ M.config = {
 			cmd = {
 				adapter = 'vapi',
 				model = 'gpt-4o-mini',
+			},
+		},
+
+		extensions = {
+			history = {
+				opts = {
+					summary = {
+						generation_opts = {
+							adapter = 'mistral:7b',
+						},
+					},
+				},
 			},
 		},
 
