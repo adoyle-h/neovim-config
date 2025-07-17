48173531df4ae4cb571d790f11285558769259c4
ADoyle <adoyle.h@gmail.com>
2025-07-17 20:38:32 +0800

    update

--- lua/my/ai.lua
+++ lua/my/ai.lua
@@ -6,6 +6,35 @@ M.plugins = {
 	-- },
 }
 
+local system_prompt = function(opts)
+	return string.format([[
+You are a highly capable, honest, and helpful AI assistant.
+Your goals are to provide accurate, relevant, and clearly structured responses in fluent, professional %s.
+
+Follow these rules:
+
+- Be concise, clear, respectful.
+- Think step by step and show reasoning when helpful.
+- Ask clarifying questions if the task is ambiguous.
+- Never fabricate information. If uncertain, clearly say so and suggest how to verify or find more details or think outside the box.
+- Always consider user’s intent. Offer useful answer rather than literal interpretation.
+- Be terse and impersonal, especially if the user responds with context outside of your tasks.
+- Response with Markdown format.
+
+When discuss code:
+
+- Start Markdown code blocks with the language name.
+- No line numbers or wrapping text in code blocks.
+- Output only code relevant to the task, and omit the code shared from user.
+
+When given a task:
+
+- Think step by step and describe the approach in pseudocode (unless told not to).
+- Only one reply per turn.
+]], opts.language)
+end
+
+
 M.config = {
 	-- avante = {
 	-- 	provider = 'openai',
@@ -32,17 +61,20 @@ M.config = {
 	codecompanion = {
 		strategies = {
 			chat = {
-				adapter = 'vapi',
-				model = 'gpt-4o-mini',
+				adapter = 'gpt-4o-mini',
 			},
 			inline = {
-				adapter = 'vapi',
-				model = 'gpt-4o-mini',
+				adapter = 'gpt-4o-mini',
 			},
 			cmd = {
-				adapter = 'vapi',
-				model = 'gpt-4o-mini',
+				adapter = 'gpt-4o-mini',
 			},
+		},
+
+		opts = {
+			log_level = 'INFO',
+			language = 'Chinese',
+			system_prompt = system_prompt,
 		},
 
 		extensions = {
@@ -75,17 +107,13 @@ M.config = {
 						api_key = os.getenv('AVANTE_OPENAI_API_KEY'),
 						chat_url = '/v1/chat/completions',
 					},
-
-					schema = {
-						model = {
-							default = 'gpt-4o-mini',
-						},
-					},
 				})
 			end,
 
 			['gpt-4o-mini'] = function()
 				return require('codecompanion.adapters').extend('vapi', {
+					name = 'gpt-4o-mini',
+					formatted_name = 'gpt-4o-mini',
 					schema = {
 						model = {
 							default = 'gpt-4o-mini',
@@ -96,6 +124,7 @@ M.config = {
 
 			['gpt-4o'] = function()
 				return require('codecompanion.adapters').extend('vapi', {
+					name = 'gpt-4o',
 					schema = {
 						model = {
 							default = 'gpt-4o',
@@ -107,6 +136,7 @@ M.config = {
 			['mistral:7b'] = function()
 				return require('codecompanion.adapters').extend('ollama', {
 					-- name = 'ollama', -- Give this adapter a different name to differentiate it from the default ollama adapter
+					name = 'mistral:7b',
 					schema = {
 						model = {
 							default = 'mistral:7b',
@@ -116,7 +146,23 @@ M.config = {
 			end,
 		},
 
+		prompt_library = {
+			['简易对话'] = {
+				strategy = 'chat',
+				description = '一次性问答',
+				prompts = {
+					{
+						role = 'system',
+						content = [[
+You are a highly capable, honest, and helpful AI assistant.
+Your goals are to provide concise, clear, accurate, relevant responses in Chinese with Markdown formatting.
+					]],
+					},
+				},
+			},
+		},
 	},
+
 }
 
 return M;
